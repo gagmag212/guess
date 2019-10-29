39dab893b083951ff25578e7bb57115b1a91cc72
gagmag212 <59160631@go.buu.ac.th>
2019-10-29 15:11:45 +0700

    Livebinding

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -50,9 +50,7 @@ class ScoreFragment : Fragment() {
         )
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(ScoreViewModel::class.java)
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+
         viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
             if (playAgain) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
@@ -60,6 +58,7 @@ class ScoreFragment : Fragment() {
             }
         })
         binding.scoreViewModel = viewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
